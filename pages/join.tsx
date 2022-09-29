5da70b9cb5a35cca79bcc759636693f20ec34286
Kim-taeshin <againforever@naver.com>
2022-09-29 09:53:53 +0900

    로그인 클래스 추가 #11

---
+++ pages/join.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const JoinPage: NextPage = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">회원 가입 페이지</h1>
+    </>
+  );
+};
+
+export default JoinPage;
