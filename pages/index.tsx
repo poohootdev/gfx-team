69248a7498a5a5765fa3fe9de6939987824004b4
poohootdev <poohoot.dev@gmail.com>
2022-11-24 16:03:39 +0900

    UI등록 팝업 Firebase 연동 #25

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useRef, useState, useEffect, FormEvent } from 'react';
+import { Fragment, useRef, useState, useEffect, FormEvent, useCallback } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
@@ -11,6 +11,8 @@ import Header from '../src/components/Header';
 import RegistButton from '../src/components/RegistButton';
 import { Dialog, Transition } from '@headlessui/react';
 import UserInfoInput from '../src/components/UserInfoInput';
+import '../src/firebase';
+import { set, ref, getDatabase } from 'firebase/database';
 
 const Home: NextPage = () => {
   interface FormElements extends HTMLFormElement {
@@ -31,8 +33,71 @@ const Home: NextPage = () => {
 
   const cancelButtonRef = useRef(null);
 
+  const [warningFla, setWarningFla] = useState('');
+  const [warningSubject, setWarningSubject] = useState('');
+  const [warningMember, setWarningMember] = useState('');
+  const [warningAPI, setWarningAPI] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const postUIData = useCallback(
+    async (fla: string, subject: string, member: string, api: string) => {
+      setError('');
+      setLoading(true);
+
+      try {
+        await set(ref(getDatabase(), 'ui/' + fla), {
+          fla: fla,
+          subject: subject,
+          member: member,
+          api: api,
+        });
+
+        setOpen(false);
+      } catch (e) {
+        const error = e as Error;
+        setError(error.message);
+        setLoading(false);
+      }
+    },
+    [],
+  );
+
   const handleSubmit = (event: FormTarget) => {
     event.preventDefault();
+
+    const fla = event.target.fla.value;
+    const subject = event.target.subject.value;
+    const member = event.target.member.value;
+    const api = event.target.api.value;
+
+    if (!fla) {
+      setWarningFla('필수 정보 입니다.');
+    } else {
+      setWarningFla('');
+    }
+
+    if (!subject) {
+      setWarningSubject('필수 정보 입니다.');
+    } else {
+      setWarningSubject('');
+    }
+
+    if (!member) {
+      setWarningMember('필수 정보 입니다.');
+    } else {
+      setWarningMember('');
+    }
+
+    if (!api) {
+      setWarningAPI('필수 정보 입니다.');
+    } else {
+      setWarningAPI('');
+    }
+
+    if (fla && subject && member && api) {
+      postUIData(fla, subject, member, api);
+    }
   };
 
   useEffect(() => {
@@ -66,6 +131,11 @@ const Home: NextPage = () => {
       </main>
       <RegistButton
         onClick={() => {
+          setWarningFla('');
+          setWarningSubject('');
+          setWarningMember('');
+          setWarningAPI('');
+
           setOpen(true);
         }}
       />
@@ -108,45 +178,91 @@ const Home: NextPage = () => {
                       <h1 className="text-3xl font-bold tracking-tight text-gray-900 mb-4">
                         UI 등록하기
                       </h1>
-                      <p>Fla 경로</p>
+
+                      <p>🔑 Fla 경로</p>
                       <UserInfoInput
-                        id={'passwordConfirm'}
-                        placeholder={'GFx_P_CustomDifficultyWidzet/PopupCustomDifficultyWidzet.fla'}
+                        id={'fla'}
+                        placeholder={'ex) upk파일명/fla파일명.fla'}
                         autoComplete={'off'}
-                      ></UserInfoInput>
-                      <p>제목</p>
-                      <UserInfoInput
-                        id={'title'}
-                        placeholder={'사용자 난이도 팝업'}
-                        autoComplete={'off'}
+                        warnigMsg={warningFla}
                       ></UserInfoInput>
 
-                      <p>담당자</p>
+                      <p>📌 제목</p>
+                      <UserInfoInput
+                        id={'subject'}
+                        placeholder={'ex) 사용자 난이도 팝업'}
+                        autoComplete={'off'}
+                        warnigMsg={warningSubject}
+                      ></UserInfoInput>
+
+                      <p>👨‍💻 담당자</p>
                       <UserInfoInput
                         id={'member'}
-                        placeholder={'홍길동'}
+                        placeholder={'ex) 홍길동'}
                         autoComplete={'off'}
+                        warnigMsg={warningMember}
                       ></UserInfoInput>
 
-                      <p>API 링크 주소</p>
+                      <p>📃 API 링크 주소</p>
                       <UserInfoInput
                         id={'api'}
                         placeholder={
-                          'https://rwiki.xxxxxx.com/display/GFX/%5BAPI%5D+GFx_P_CustomDifficultyWidzet.upk'
+                          'ex) https://rwiki.xxxxxx.com/display/GFX/%5BAPI%5D+GFx_P_CustomDifficultyWidzet.upk'
                         }
                         autoComplete={'off'}
+                        warnigMsg={warningAPI}
                       ></UserInfoInput>
                     </div>
 
-                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                      <button
-                        id="submit"
-                        type="submit"
-                        className="inline-flex w-full justify-center rounded-md border border-transparent bg-gray-900 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-gray-700 sm:ml-3 sm:w-auto sm:text-sm"
-                        onClick={() => setOpen(false)}
+                    {error ? (
+                      <div
+                        className="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                        role="alert"
                       >
-                        등록
-                      </button>
+                        <span className="block sm:inline">{error}</span>
+                      </div>
+                    ) : null}
+
+                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                      {!loading ? (
+                        <button
+                          id="submit"
+                          type="submit"
+                          className="inline-flex w-full justify-center rounded-md border border-transparent bg-gray-900 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-gray-700 sm:ml-3 sm:w-auto sm:text-sm"
+                        >
+                          등록
+                        </button>
+                      ) : null}
+
+                      {loading ? (
+                        <button
+                          type="button"
+                          className="inline-flex w-full justify-center rounded-md border border-transparent bg-gray-900 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-gray-700 sm:ml-3 sm:w-auto sm:text-sm"
+                        >
+                          <svg
+                            className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <circle
+                              className="opacity-25"
+                              cx="12"
+                              cy="12"
+                              r="10"
+                              stroke="currentColor"
+                              strokeWidth="4"
+                            ></circle>
+                            <path
+                              className="opacity-75"
+                              fill="currentColor"
+                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                            ></path>
+                          </svg>
+                          처리중...
+                        </button>
+                      ) : null}
+
                       <button
                         type="button"
                         className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
